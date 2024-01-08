e4ce0b2d17f45cd4d6201fd8f5695eb1a0a9e8fc
Antoine LASSERRE <98165865+Heronal@users.noreply.github.com>
2024-01-08 15:59:50 +0100

    Update acquisition_camera.py
    acqui_camera

---
+++ src/Codes/Camera/acquisition_camera.py
@@ -0,0 +1,6 @@
+import subprocess
+
+
+def acquisition_camera():
+    print("Lancement de l'acquisition de la camera")  # Affichez le message dans la console
+    subprocess.Popen("/opt/ros/noetic/bin/rosbag record --duration 1 -o /home/icam/kuhn_ws/src/ihm_krunk/src/acquisitions/acquisition_camera/ /pylon_camera_node/image_raw", shell=True)
