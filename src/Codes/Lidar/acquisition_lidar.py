6285a06e5b4f854311340a9247daacf46f49440d
Antoine LASSERRE <98165865+Heronal@users.noreply.github.com>
2024-01-08 11:47:28 +0100

    Update acquisition_lidar.py
    change name function

--- src/Codes/Lidar/acquisition_lidar.py
+++ src/Codes/Lidar/acquisition_lidar.py
@@ -1,6 +1,6 @@
 import subprocess
 
 
-def acquisition_rplidar():
+def acquisition_r2000():
     print("Lancement de l'acquisition du rplidar")  # Affichez le message dans la console
     subprocess.Popen("/opt/ros/noetic/bin/rosbag record --duration 1 -o /home/icam/kuhn_ws/src/ihm_krunk/src/acquisitions/acquisition_r2000/ /scan", shell=True)
